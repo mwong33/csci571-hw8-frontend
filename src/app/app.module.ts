3db0201c54c4bc9a3eb4cad579109266b55722c5
Matthew James Wong <matthew.wong76@gmail.com>
2020-06-14 18:00:44 -0600

    First real commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchItemsComponent } from './components/search-items/search-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchItemsComponent
   ],
   imports: [
     BrowserModule,
