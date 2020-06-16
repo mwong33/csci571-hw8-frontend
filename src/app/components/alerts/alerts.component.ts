490fbe5ff8653dcaf132def2e63f40fd2f7e36ac
Matthew James Wong <matthew.wong76@gmail.com>
2020-06-15 22:04:41 -0600

    alerts use string property to display message

--- src/app/components/alerts/alerts.component.ts
+++ src/app/components/alerts/alerts.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertsComponent implements OnInit {
 
+  alertMessage:string = "Hello There!";
+
   constructor() { }
 
   ngOnInit(): void {
